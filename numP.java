68c0582c4e13d57470bb40cd43213718ffd4cf60
Jesus31607 <roberjesus190@gmail.com>
2025-10-23 22:05:33 -0600

    Add files via upload

--- numP.java
+++ numP.java
@@ -1,23 +1,29 @@
 import java.util.Scanner;
-public class numP{
-	public static void main(String args[]){
-	Scanner sc = new Scanner(System.in);
-	int numN,contador=0,i;
-	System.out.println("ingrese un valor");
-	numN=sc.nextInt();
 
-	if(numN <= 1){
-	System.out.println("El numero ingresado: " + numN+ " no es primo");
-	}else{
-	for(i=1; i<=numN; i++){
-	if(numN % i ==0){
-	contador++;
-	if(contador==2)
-	System.out.println("EL numero ingresado: "+numN + " Es primo");
-	}else{ 
-	System.out.println("El numero ingresado: "+numN + " No es primo");
-}
-}
-	}	
-}
+public class NumP {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese un número: ");
+        int n = scanner.nextInt();
+
+        boolean primo;
+
+        if (n <= 1) {
+            primo = false;
+        } else if (n == 2 || n == 3 || n == 5 || n == 7) {
+            primo = true;
+        } else if (n % 2 == 0 || n % 3 == 0 || n % 5 == 0 || n % 7 == 0) {
+            primo = false;
+        } else {
+            primo = true;
+        }
+
+        if (primo) {
+            System.out.println("El número " + n + " es primo.");
+        } else {
+            System.out.println("El número " + n + " no es primo.");
+        }
+
+        scanner.close();
+    }
 }
